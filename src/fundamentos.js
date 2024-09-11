8a3c24af6ba5130177984b8cca21a62496e9ca8c
linhareshian <hian.linhares@rethink.dev>
2024-09-10 21:26:37 -0300

    primeira aula do NLW Javascript

---
+++ src/fundamentos.js
@@ -0,0 +1,16 @@
+// let metas = ["Sim", "Não"]
+// console.log(metas[0] + " "+ metas[1])
+
+// // const soma = (a,b) => {
+// //     return a+b
+// // }
+
+let carro = {
+    preco: 26000,
+    financiamento: true,
+    meunome: () => {
+        return "Hian"
+    }
+}
+
+console.log(carro.preco)
---
+++ src/index.js
@@ -0,0 +1,20 @@
+let metas = {
+    value: "ler um livro",
+    checked: true,
+}
+
+let metasArray = [ 
+    {
+        meta: "ir a academia 4 dias na semana",
+        checked: false,
+        data_limited: "10/09/2024"
+    } ,
+    {
+        meta: "correr 4 dias na semana",
+        checked: false,
+        data_limited: "15/09/2024"
+    } 
+]
+
+
+console.log(metasArray)
